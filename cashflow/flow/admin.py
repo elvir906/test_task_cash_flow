bed53175f5d5c9d964c0aaaefab7647592f80f84
Elvir Davletgareev <davletelvir@Elvirs-MacBook-Pro.local>
2025-09-29 23:09:19 +0300

    63/12

--- cashflow/flow/admin.py
+++ cashflow/flow/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from .models import Field
+from .models import Field, Status
 
 class FieldAdmin(admin.ModelAdmin):
   list_display = ['pub_date', 'status']
 
 
+class StatusAdmin(admin.ModelAdmin):
+  list_display = ['val']
+
+
 admin.site.register(Field, FieldAdmin)
+admin.site.register(Status, StatusAdmin)
--- cashflow/flow/models.py
+++ cashflow/flow/models.py
@@ -2,16 +2,17 @@ from django.db import models
 
 
 class Status(models.Model):
-  BUSSINES = 'B'
-  PERSONAL = 'P'
-  TAX = 'T'
-  status_list = [(BUSSINES, 'Бизнес'), (PERSONAL, 'Личное'), (TAX, 'Налог'),]
-  title = models.CharField(max_length=200, choices=status_list)
+  val = models.CharField(
+    max_length=100,
+    blank=False,
+    default='Bussiness',
+    verbose_name='Статусы',
+  )
 
 class Field(models.Model):
   pub_date = models.DateTimeField(auto_now_add=True, editable=True)
   status = models.ForeignKey(
     Status,
-    related_name='status',
+    related_name='statuses',
     on_delete=models.CASCADE,
   )
