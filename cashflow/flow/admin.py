44c8fe3c5d5c1bb13294e3d5f12e508609cb1006
elvir906 <himynameiselvir@gmail.com>
2025-09-30 13:12:36 +0300

    dell

--- cashflow/flow/admin.py
+++ cashflow/flow/admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
 
-from .models import Field, Status
+from .models import Field, Status, CustomStatus
 
 class FieldAdmin(admin.ModelAdmin):
   list_display = ['pub_date', 'status']
 
 
 class StatusAdmin(admin.ModelAdmin):
-  list_display = ['val']
+  list_display = ['value']
+
+
+class CustomStatusAdmin(admin.ModelAdmin):
+  list_display = ['value']
 
 
 admin.site.register(Field, FieldAdmin)
 admin.site.register(Status, StatusAdmin)
+admin.site.register(CustomStatus, CustomStatusAdmin)
--- cashflow/flow/models.py
+++ cashflow/flow/models.py
@@ -2,17 +2,60 @@ from django.db import models
 
 
 class Status(models.Model):
-  val = models.CharField(
+  class Choice_list(models.TextChoices):
+    B = ('Бизнес', 'Бизнес')
+    P = ('Личное', 'Личное')
+    T = ('Налоги', 'Налоги')
+
+  value = models.CharField(
+    choices=Choice_list.choices,
+    editable=True,
     max_length=100,
-    blank=False,
-    default='Bussiness',
-    verbose_name='Статусы',
+    unique=True,
+    null=False,
+    verbose_name='Наименование статуса'
   )
 
+  class Meta:
+      verbose_name = 'Статус'
+      verbose_name_plural = 'Статусы'
+
+  def __str__(self) -> str:
+    return self.value
+
+
+class CustomStatus(models.Model):
+    value = models.CharField(
+    editable=True,
+    max_length=100,
+    unique=True,
+    null=False,
+    verbose_name='Наименование нового статуса'
+  )
+
+    def __str__(self) -> str:
+      return self.value
+
+
 class Field(models.Model):
-  pub_date = models.DateTimeField(auto_now_add=True, editable=True)
+  pub_date = models.DateField(
+    auto_now_add=True,
+    editable=True,
+    verbose_name='Дата публикации'
+  )
   status = models.ForeignKey(
     Status,
-    related_name='statuses',
+    # related_name='statuses',
     on_delete=models.CASCADE,
+    null=False,
+    editable=True,
+    verbose_name='Категория ДДС'
   )
+
+  class Meta:
+      verbose_name = 'Запись ДДС'
+      verbose_name_plural = 'Записи ДДС'
+      ordering = ['pub_date']
+
+  def __str__(self) -> str:
+    return self.status.value
